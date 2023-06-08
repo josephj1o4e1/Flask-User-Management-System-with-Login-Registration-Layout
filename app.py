055dda54225754c137c20739a48fcce2281fe6dc
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-06-08 14:51:00 -0400

    wtforms mild add forms.py

--- app.py
+++ app.py
@@ -14,16 +14,7 @@ db = SQLAlchemy(app)
 
 # import db schema
 from models import *
-
-# flask-wtf forms
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+from forms import LoginForm
 
 # login required decorator
 def login_required(f):
