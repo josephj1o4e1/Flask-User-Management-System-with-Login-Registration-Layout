0b12a9b085fa7f5187785d8808f840fd2d007dab
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-06-10 04:29:30 -0400

    modified db_create.py and db_create_user.py

--- db_create.py
+++ db_create.py
@@ -1,21 +1,28 @@
 from app import app
 from app import db
-from models import BlogPost
+from models import BlogPost, User
 
 with app.app_context(): # to establish an application context. It ensures that the necessary application context is set up before interacting with the database using `db.session`.
-    # drop tables for the default bind
-    db.drop_all(bind_key=None)
+    # # drop tables for the default bind
+    # db.drop_all(bind_key=None)
     
     # create the database and the db table
     # initializes the database based on the schema we defined in models.py
     db.create_all()
     
     # insert data
-    db.session.add(BlogPost("Good", "I\'m good."))
-    db.session.add(BlogPost("Well", "I\'m well."))
-    db.session.add(BlogPost("Excellent", "I\'m excellent."))
-    db.session.add(BlogPost("Okay", "I\'m okay."))
-    db.session.add(BlogPost("Okay2", "I\'m okay 2."))
+    user_curry=User.query.filter_by(name='StephCurry').first()
+    user_ohtani=User.query.filter_by(name='ShoheiOhtani').first()
+    user_durant=User.query.filter_by(name='KevinDurant').first()
+    b1 = BlogPost("Good", "I\'m good.")
+    b1.author=user_curry
+    b2 = BlogPost("Well", "I\'m well.")
+    b2.author=user_ohtani
+    b3 = BlogPost("Excellent", "I\'m excellent.")
+    b3.author=user_durant
+    db.session.add(b1)
+    db.session.add(b2)
+    db.session.add(b3)
 
     # commit the changes
     db.session.commit()
--- db_create_user.py
+++ db_create_user.py
@@ -4,8 +4,14 @@ from models import User
 
 with app.app_context(): 
     # insert data
-    db.session.add(User("michael", "michael@realpython.com", "i'll-never-tell"))
-    db.session.add(User("admin", "ad@min.com", "admin"))
+    db.session.add(User("JosephChuang", "chuang@iu.edu", "josephchuang"))
+    db.session.add(User("StephCurry", "curry@gmail.com", "stephcurry"))
+    db.session.add(User("BabeRuth", "ruth@gmail.com", "baberuth"))
+    db.session.add(User("TomBrady", "brady@hotmail.com", "tombrady"))
+    db.session.add(User("NikolaJokic", "jokic@yahoo.com", "nikolajokic"))
+    db.session.add(User("ShoheiOhtani", "ohtani@gmail.com", "shoheiohtani"))
+    db.session.add(User("KevinDurant", "durant@hotmail.com", "kevindurant"))
+    # db.session.add(User("admin", "ad@min.com", "admin"))
 
     # commit the changes
     db.session.commit()
