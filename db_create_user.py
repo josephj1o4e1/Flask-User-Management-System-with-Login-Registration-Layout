d881f69a4bae0566aa7e200ff5efe8e79b550989
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-06-18 17:11:17 -0400

    db_create_user

---
+++ db_create_user.py
@@ -0,0 +1,18 @@
+from app import app
+from app import db
+from models import User
+
+with app.app_context(): 
+    # insert data for quick demo
+    db.session.add(User("JosephChuang", "chuang@iu.edu", "josephchuang"))
+    db.session.add(User("StephCurry", "curry@gmail.com", "stephcurry"))
+    db.session.add(User("BabeRuth", "ruth@gmail.com", "baberuth"))
+    db.session.add(User("TomBrady", "brady@hotmail.com", "tombrady"))
+    db.session.add(User("NikolaJokic", "jokic@yahoo.com", "nikolajokic"))
+    db.session.add(User("ShoheiOhtani", "ohtani@gmail.com", "shoheiohtani"))
+    db.session.add(User("KevinDurant", "durant@hotmail.com", "kevindurant"))
+    # db.session.add(User("admin", "ad@min.com", "admin"))
+    # db.session.add(User("Bill Gates", "gates@hotmail.com", "Microsoft1#"))
+
+    # commit the changes
+    db.session.commit()
