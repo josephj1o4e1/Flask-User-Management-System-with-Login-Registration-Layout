44df3ab3023740d065ac275f21b5096ebbd56d78
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2023-10-30 22:50:20 -0400

    Revert "db_create_user"
    This reverts commit 934d301c6916189fa9c2f668b711ae8a9309be73.

--- app/LoginApp/db_create_user.py
+++ app/LoginApp/db_create_user.py
@@ -6,14 +6,14 @@ with current_app.app_context():
     # Delete all data rows in table "users" including the referenced data such as in the table "posts". 
     db.session.execute(db.text('TRUNCATE TABLE users CASCADE'))
     # insert data for quick demo
-    db.session.add(User(name="JosephChuang", username="chuang@iu.edu", email="chuang@iu.edu", password="josephchuang"))
-    db.session.add(User(name="JosephChuang", username="JosephChuang123", email="chuang2@iu.edu", password="josephchuang"))
-    db.session.add(User(name="StephCurry", username="StephCurry", email="curry@gmail.com", password="stephcurry"))
-    db.session.add(User(name="BabeRuth", username="BabeRuth", email="ruth@gmail.com", password="baberuth"))
-    db.session.add(User(name="TomBrady", username="TomBrady", email="brady@hotmail.com", password="tombrady"))
-    db.session.add(User(name="NikolaJokic", username="NikolaJokic", email="jokic@yahoo.com", password="nikolajokic"))
-    db.session.add(User(name="ShoheiOhtani", username="ShoheiOhtani", email="ohtani@gmail.com", password="shoheiohtani"))
-    db.session.add(User(name="KevinDurant", username="KevinDurant", email="durant@hotmail.com", password="kevindurant"))
+    db.session.add(User("JosephChuang", "chuang@iu.edu", "chuang@iu.edu", "josephchuang"))
+    db.session.add(User("JosephChuang", "JosephChuang123", "chuang2@iu.edu", "josephchuang"))
+    db.session.add(User("StephCurry", "StephCurry", "curry@gmail.com", "stephcurry"))
+    db.session.add(User("BabeRuth", "BabeRuth", "ruth@gmail.com", "baberuth"))
+    db.session.add(User("TomBrady", "TomBrady", "brady@hotmail.com", "tombrady"))
+    db.session.add(User("NikolaJokic", "NikolaJokic", "jokic@yahoo.com", "nikolajokic"))
+    db.session.add(User("ShoheiOhtani", "ShoheiOhtani", "ohtani@gmail.com", "shoheiohtani"))
+    db.session.add(User("KevinDurant", "KevinDurant", "durant@hotmail.com", "kevindurant"))
 
     # db.session.add(User("Bill Gates", "Bill Gates", "gates@hotmail.com", "Microsoft1#"))
     # Zz00000#
